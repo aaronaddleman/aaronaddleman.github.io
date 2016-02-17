a043a1073b6b01ea2e572f1d1c5dc1e7b45c790c
Aaron Addleman <aaronaddleman@gmail.com>
2016-02-16 20:30:17 -0800

    set article img to be responsive

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.container img').addClass( 'responsive-img' );
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
